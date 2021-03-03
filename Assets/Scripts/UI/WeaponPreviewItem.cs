e6c137b3cdc6ba9a84fc50d1c30b4e73fd8e6f68
david.zhu <david.zhu@digitwin.com.cn>
2021-03-03 18:51:15 +0800

    *upload

--- Assets/Scripts/UI/WeaponPreviewItem.cs
+++ Assets/Scripts/UI/WeaponPreviewItem.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponPreviewItem : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Text m_Name;
+    public Text m_ShotRange;
+    Weapon m_Weapon;
+
+    public Weapon WeaponItem {
+        set { 
+            m_Weapon = value;
+            m_Name.text = m_Weapon.Name;
+            m_ShotRange.text = (m_Weapon.WeaponShutRange * 100).ToString();
+        }
+        get { return m_Weapon; }
+    } 
     void Start()
     {
         
--- Assets/Scripts/UI/WeaponSwitchPanel.cs
+++ Assets/Scripts/UI/WeaponSwitchPanel.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class WeaponSwitchPanel : MonoBehaviour
 {
-    public GameObject m_ListContent;
+    public GameObject m_Viewport;
     public GameObject m_PreviewItem;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void updateItemList()
     {
         
+        for(int i=0; i< m_Viewport.transform.childCount; ++i)
+        {
+            var child = m_Viewport.transform.GetChild(i);
+            Destroy(child);
+        }
+
+        List<Weapon> weapons = new List<Weapon>();
+        weapons.Add(new LightMachineGun());
+        weapons.Add(new MissileStandard01());
+        float height = 0;
+        for (int i=0; i< weapons.Count; ++i)
+        {
+            var objItem = Instantiate<GameObject>(m_PreviewItem);
+            objItem.transform.SetParent(transform);
+
+            var perviewItem = objItem.GetComponent<WeaponPreviewItem>();
+            perviewItem.WeaponItem = weapons[i];
+            height += perviewItem.GetComponent<RectTransform>().rect.height;
+        }
+        var rect = m_Viewport.GetComponent<RectTransform>().rect.height;
     }
 }
